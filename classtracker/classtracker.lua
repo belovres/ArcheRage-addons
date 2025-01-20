2c84463338bcf1093b87437f10677c0127d32009
Schiz-n <Noiryxp@hotmail.com>
2025-01-20 22:20:40 +0100

    Add a few more classes to classtracker

--- classtracker/classtracker.lua
+++ classtracker/classtracker.lua
@@ -85,27 +85,40 @@ initializeIcons(buffAnchor)
 
 local nameMappings = {
     ["name_3_4_5"] = "Tank", -- Skullknight
+    ["name_2_3_4"] = "Tank", -- Dreambreaker
     ["name_1_5_8"] = "Melee", -- Executioner
     ["name_1_3_5"] = "Melee", -- Doomlord
     ["name_1_8_9"] = "Melee", -- Blade Dancer
     ["name_1_3_4"] = "Melee", -- Abolisher
     ["name_1_4_8"] = "Melee", -- Darkrunner
+    ["name_1_4_9"] = "Melee", -- Herald
     ["name_1_8_12"] = "Swiftblade", -- Deathwish
+    ["name_1_5_12"] = "Swiftblade", -- Deathwish
     ["name_7_8_11"] = "Malediction", -- Fanatic
     ["name_7_9_11"] = "Malediction", -- Spectre
-    ["name_7_8_9"] = "Mage", -- Fanatic
+    ["name_7_8_9"] = "Mage", -- I forgot
+    ["name_4_7_8"] = "Mage", -- Enigmatist
+    ["name_3_4_7"] = "Mage", -- Thaumaturge
+    ["name_2_7_8"] = "Mage", -- Daggerspell
     ["name_6_8_9"] = "Archer", -- Ebonsong
+    ["name_6_9_10"] = "Archer", -- Soulsong
+    ["name_2_6_9"] = "Archer", -- Hex Ranger
     ["name_6_8_13"] = "Gunner", -- Deathtrigger
     ["name_4_8_13"] = "Gunner", -- Bounty Hunter
     ["name_5_6_13"] = "Gunner", -- Banebolt
     ["name_8_9_13"] = "Gunner", -- Privateer
+    ["name_4_9_13"] = "Gunner", -- Minstrel
     ["name_3_4_9"] = "Songer", -- Tomb Warden
     ["name_9_10_14"] = "Dancer", -- Glamorous Savior
     ["name_8_10_14"] = "Dancer", -- Darkness Savior
+    ["name_2_10_14"] = "Dancer", -- Fear Savior
     ["name_8_9_14"] = "Dancer", -- Bloody Dancer
     ["name_2_8_10"] = "Healer", -- Assassin
+    ["name_4_8_10"] = "Healer", -- Soothsayer
+    ["name_2_9_10"] = "Healer", -- Athame
     ["name_8_9_10"] = "Healer", -- Confessor
-    ["name_3_9_10"] = "Healer"-- Caretaker
+    ["name_3_9_10"] = "Healer",-- Caretaker
+    ["name_2_4_10"] = "Healer"-- Hierophant
 }
 
 local function hideNonMatchingIcons(currentClassName)
