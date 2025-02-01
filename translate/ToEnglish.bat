c4c3d05d2c0257dfd41599d4e35dde5187e06e7f
Schiz-n <Noiryxp@hotmail.com>
2025-02-01 18:47:44 +0100

    More user error guardrails

--- translate/ToEnglish.bat
+++ translate/ToEnglish.bat
@@ -2,7 +2,7 @@
 set LANG=en
 
 :START
-echo Started script succesfully, please check in-game...
+echo Started script succesfully, please check in-game. DO NOT CLOSE THIS WINDOW.
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "translatetofilep2.ps1" -lang %LANG%
 if %ERRORLEVEL% NEQ 0 (
     echo PowerShell script exited with an error. Restarting...
