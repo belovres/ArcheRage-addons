c957452e833a7de7d60a70c3fcab54a88a482fda
Schiz-n <Noiryxp@hotmail.com>
2025-02-15 03:49:14 +0100

    Major cleanup + save window position

--- titleswap/titleswap.lua
+++ titleswap/titleswap.lua
@@ -1,5 +1,6 @@
 -------------- Original Author: Strawberry --------------
 ----------------- Discord: exec.noir --------------------
+------------- Thanks to Pinkl and Raikiri ---------------
 ADDON:ImportObject(OBJECT_TYPE.TEXT_STYLE)
 ADDON:ImportObject(OBJECT_TYPE.BUTTON)
 ADDON:ImportObject(OBJECT_TYPE.DRAWABLE)
@@ -50,13 +51,11 @@ local buttonskin = {
         bottom = 0,
     },
 }
-
 local color_selected = {}
 color_selected.normal    = UIParent:GetFontColor("green")
 color_selected.highlight = UIParent:GetFontColor("green")
 color_selected.pushed    = UIParent:GetFontColor("green")
 color_selected.disabled  = UIParent:GetFontColor("btn_dis")
-
 local buttonskin_selected = {
     drawableType = "ninePart",
     path = "ui/common/default.dds",
@@ -72,31 +71,51 @@ local buttonskin_selected = {
 }
 
 -- actual title box
-local titleWindow = CreateEmptyWindow("titleWindow", "UIParent")
-titleWindow:Show(true)
-
-local titles = {}  -- Stores loaded titles in memory
+local titles = {}
 local titlesFile = "titles.lua"
 
--- Create the title list window
 local titleListWindow = CreateEmptyWindow("titleListWindow", "UIParent")
-titleListWindow:SetExtent(0, 0) -- Set size (width, height)
-titleListWindow:AddAnchor("RIGHT", "UIParent", -100, -200) -- Center the window
+titleListWindow:SetExtent(0, 0)
+--titleListWindow:AddAnchor("RIGHT", "UIParent", -100, -200)
 titleListWindow:EnableDrag(true)
 titleListWindow:Show(true)
 
---thanks pinkl
+
+local titleWidgets = {}
+
+local filePath = "TitleWindowPos.txt"
+local function SaveWindowPosition(x, y)
+    local file = io.open(filePath, "w")
+    file:write(string.format("%d,%d", x, y))
+    file:close()
+end
+local function LoadSavedPosition()
+    local file = io.open(filePath, "r")
+    if not file then
+        return 0, 0
+    end
+    local line = file:read("*line") 
+    file:close()
+    local x,y = line:match("(%d+),(%d+)")
+    if x and y then
+        return x,y
+    else
+        return 0,0
+    end
+end
+
+local savedWindowX, savedWindowY = LoadSavedPosition()
+--X2Chat:DispatchChatMessage(CMF_SYSTEM, "Putting window at: " .. tostring(savedWindowX) .. tostring(savedWindowY))
+titleListWindow:AddAnchor("TOPLEFT", "UIParent", tonumber(savedWindowX), tonumber(savedWindowY))
+
 local background = titleListWindow:CreateColorDrawable(0, 0, 0, 0.5, "background")
 background:AddAnchor("TOPLEFT", titleListWindow, 0, 0)
 background:AddAnchor("BOTTOMRIGHT", titleListWindow, 0, 0)
 
-local titleWidgets = {} -- Store buttons for updating later
-
--- Reads titles from the file
 local function initializeTitles()
     local file = io.open(titlesFile, "r")
     if file then
-        titles = {}  -- Reset the table to avoid duplicates
+        titles = {}
         for line in file:lines() do
             local id, name, path = line:match('%["(%d+)"%]%s*=%s*{name%s*=%s*"(.-)",%s*icon%s*=%s*"(.-)"}')
             if id and name and path then
@@ -107,8 +126,6 @@ local function initializeTitles()
     else
         X2Chat:DispatchChatMessage(CMF_SYSTEM, "Title file not found, initializing empty list.")
     end
-
-    -- Generate UI buttons here using titles[id].name and titles[id].icon
 end
 
 --these handle title setting from buttons:
@@ -127,9 +144,9 @@ local function createTitleList()
     -- Clear existing buttons
     for _, widget in ipairs(titleWidgets) do
         widget:SetText("")
-        widget:AddAnchor("TOPLEFT", titleListWindow, 9999, 9999) -- Move out of view temporarily
+        widget:AddAnchor("TOPLEFT", titleListWindow, 9999, 9999) -- don't think about this too much :)
     end
-    titleWidgets = {}  -- Reset the table of title widgets
+    titleWidgets = {}
 
     local yOffset = 10
     local buttonSpacing = 25
@@ -139,16 +156,14 @@ local function createTitleList()
     local currentTitle = X2Player:GetEffectAppellation()
     local currentTitleId = tostring(currentTitle[1])
 
-    local totalTitles = 0  -- Keep track of how many titles we've processed
-    -- Loop through titles and create buttons
+    local totalTitles = 0 
     for id, data in pairs(titles) do
-        totalTitles = totalTitles + 1  -- Increment the number of titles
+        totalTitles = totalTitles + 1
 
         local titleButton = titleListWindow:CreateChildWidget("button", "titleButton_" .. id, tonumber(id), true)
         titleButton:AddAnchor("TOPLEFT", titleListWindow, 10, yOffset)
         titleButton:SetText(data.name)
 
-        -- Apply the appropriate skin based on the current title
         if id == currentTitleId then
             ApplyButtonSkin(titleButton, buttonskin_selected)
         else
@@ -172,13 +187,12 @@ local function createTitleList()
         end)
 
         table.insert(titleWidgets, titleButton)
-        yOffset = yOffset + buttonSpacing  -- Move down for the next button
+        yOffset = yOffset + buttonSpacing  
     end
 
-    -- Set the extent (size) of the window based on the number of titles
-    local windowWidth = 100  -- Adjusted width for the window (slightly bigger than button width)
-    local windowHeight = totalTitles * buttonSpacing + 25  -- Height of the window, taking spacing into account
-    titleListWindow:SetExtent(windowWidth, windowHeight)  -- Set the size of the window
+    local windowWidth = 100
+    local windowHeight = totalTitles * buttonSpacing + 25  
+    titleListWindow:SetExtent(windowWidth, windowHeight) 
 end
 
 
@@ -201,17 +215,23 @@ end
 local function saveTitle(titleId, titleName, titleIconPath)
     titles[titleId] = {name = titleName, icon = titleIconPath}
     saveTitles()
-    X2Chat:DispatchChatMessage(CMF_SYSTEM, "Saved title: " .. titleName)
+    --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Saved title: " .. titleName)
 end
 
 local function deleteTitle(titleId, name)
-    --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Current titles: " .. dump(titles))
-    --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Checking for : " .. titleId)
-    --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Which should be: " .. dump(titles[titleId]))
-    if titles[titleId] then--and titles[titleId].name == name then
+    local titleFound = false
+    for id, data in pairs(titles) do
+        if data.name == name then
+            titleFound = true
+            titleId = id 
+            break
+        end
+    end
+
+    if titleFound then
         titles[titleId] = nil
         saveTitles()
-        X2Chat:DispatchChatMessage(CMF_SYSTEM, "Deleted title: " .. name)
+        --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Deleted title: " .. name)
     else
         X2Chat:DispatchChatMessage(CMF_SYSTEM, "Title not found: " .. name)
     end
@@ -243,10 +263,13 @@ local chatAggroEventListenerEvents = {
                 end
                 --X2Chat:DispatchChatMessage(CMF_SYSTEM, "Removing title." .. titleId .. titleName)
                 deleteTitle(titleId, titleName)
+--            elseif firstWord == "/movewindow" then
+--                titleListWindow:EnableDrag(true)
             end
         end
     end
 }
+
 
 --make chat listener
 local chatEventListenerAggro = CreateEmptyWindow("chatEventListenerAggro", "UIParent")
@@ -261,7 +284,7 @@ local RegistUIEvent = function(window, eventTable)
 end
 RegistUIEvent(chatEventListenerAggro, chatAggroEventListenerEvents)
 
---make draggable (needs save function)
+--make draggable
 function titleListWindow:OnDragStart()
     self:StartMoving()
     self.moving = true
@@ -271,11 +294,16 @@ titleListWindow:SetHandler("OnDragStart", titleListWindow.OnDragStart)
 function titleListWindow:OnDragStop()
     self:StopMovingOrSizing()
     self.moving = false
+    local offsetX, offsetY = self:GetOffset()
+    local uiScale = UIParent:GetUIScale() or 1.0
+    local normalizedX = offsetX * uiScale
+    local normalizedY = offsetY * uiScale
+    SaveWindowPosition(normalizedX, normalizedY)
 end
 titleListWindow:SetHandler("OnDragStop", titleListWindow.OnDragStop)
 
 
-X2Chat:DispatchChatMessage(CMF_SYSTEM, "Initializing Titleswap.")
+--X2Chat:DispatchChatMessage(CMF_SYSTEM, "Initializing Titleswap.")
 initializeTitles()
 createTitleList()
 X2Chat:DispatchChatMessage(CMF_SYSTEM, "Titleswap loaded successfully.")
